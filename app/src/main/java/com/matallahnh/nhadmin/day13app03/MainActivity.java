94b6ef50631a79637e75cf7a861adce4ef084288
AtallahM <32632750+AtallahM@users.noreply.github.com>
2018-03-21 23:29:00 +0300

    new commint

--- app/src/main/java/com/matallahnh/nhadmin/day13app03/MainActivity.java
+++ app/src/main/java/com/matallahnh/nhadmin/day13app03/MainActivity.java
@@ -17,6 +17,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 //some changes
+//new changes
 public class MainActivity extends AppCompatActivity {
     private final String SERVIVCE_URL = "http://apilayer.net/api/live?access_key=089debb1ad610a91fa2c760b5b4d004a&source=USD&currencies=USD,QAR,EGP,GBP&format=1";
     private TextView txt ;
